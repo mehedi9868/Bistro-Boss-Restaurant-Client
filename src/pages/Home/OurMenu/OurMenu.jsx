4403a84fbe1bd1b183869241c97dbc9386b658fd
mehedi9868 <mehedi9868@gmail.com>
2023-05-27 16:37:59 +0600

    dynamic title added using react helmate

---
+++ src/pages/Home/OurMenu/OurMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+
+const OurMenu = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Bistro Boss | Menu</title>
+            </Helmet>
+        </div>
+    );
+};
+
+export default OurMenu;
